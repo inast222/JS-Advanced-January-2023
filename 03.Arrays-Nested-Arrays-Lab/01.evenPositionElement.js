71cb4a66876d068666b4d0972c79a668f4408178
inast222 <inast222@gmail.com>
2023-01-20 21:13:43 +0200

    Add more exercise

---
+++ 03.Arrays-Nested-Arrays-Lab/01.evenPositionElement.js
@@ -0,0 +1,11 @@
+function evenPositionElements(array) {
+    let result = [];
+
+    for (let i = 0; i < array.length; i++) {
+        if (i % 2 === 0) {
+            result.push(array[i]);
+        }
+    }
+    console.log(result.join(' '));
+}
+evenPositionElements(['20', '30', '40', '50', '60'])
